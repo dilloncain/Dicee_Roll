21ce1634fb7a0665120193d9018606f7cca6eb5a
Dillon Cain <dilloncain@caincomputer.com>
2019-03-18 22:35:13 -0700

    Initial Commit

---
+++ Dicee Roll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee Roll
+//
+//  Created by Dillon Cain on 3/18/19.
+//  Copyright © 2019 Cain Computers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
